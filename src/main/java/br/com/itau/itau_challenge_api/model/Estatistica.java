dc4a64f97940b9df32dbfe0ae4b7ffc6ff7126f9
tulio <tulio.jacinto32@gmail.com>
2025-04-21 02:13:00 -0300

    feat: Adicionando Endpoint calcular Estatisticas

---
+++ src/main/java/br/com/itau/itau_challenge_api/model/Estatistica.java
@@ -0,0 +1,4 @@
+package br.com.itau.itau_challenge_api.model;
+
+public record Estatistica(Double count, Double sum, Double avg, Double min, Double max) {
+}
---
+++ src/test/java/br/com/itau/itau_challenge_api/ItauChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.itau.itau_challenge_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
